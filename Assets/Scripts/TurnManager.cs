8d9363dd789db06ecd174b8ec130553200901b54
Aaron Hsieh <hsieh.361@buckeyemail.osu.edu>
2025-05-23 21:39:32 -0400

    Player object creation

---
+++ Assets/Scripts/TurnManager.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class TurnManager : MonoBehaviour
+{
+    #region Constants
+    const int turn Activation
+    #endregion
+
+    #region Notifications
+    public const string RoundBeganNotificaiton = "TurnManager.roundBegan";
+    #endregion
+
+    #region Public
+
+    #endregion
+}
